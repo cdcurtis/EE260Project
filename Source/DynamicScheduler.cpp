e03a65aec92a6557a660533d42c1df92393cd36b
Joshua <jpott002@ucr.edu>
2015-02-10 16:46:36 -0800

    End of day 20150210

--- Source/DynamicScheduler.cpp
+++ Source/DynamicScheduler.cpp
@@ -1,20 +1,65 @@
 #include "../Headers/DynamicScheduler.h"
 
-/*
-	Default Constructer
-*/
+/* ==========================================================================
+	Default Constructor
+	
+	@params
+	@returns
+	========================================================================== */
 DynamicScheduler::DynamicScheduler() {
-
+	// Default
 }
 
-/*
+/* ==========================================================================
 	Constructer with vector of DagGens passed in 
-*/
+	
+	@params
+	@returns
+	========================================================================== */
 DynamicScheduler::DynamicScheduler(std:: vector<DagGen> dgAssays) {
 	// Initialize DynamicScheduler Object
+	// intt LeveledDAGs
 }
 
-DynamicScheduler::~DynamicScheduler(); {
+/* ==========================================================================
+	Destructor
+	
+	@params
+	@returns
+	========================================================================== */
+DynamicScheduler::~DynamicScheduler() {
 	// Destructor
 	std:: cout << "DynamicScheduler destructor called.\n";
+}
+
+/* ==========================================================================
+	Master schedule caller
+	
+	@params		ScheduleType
+	@returns		int				status code
+	========================================================================== */
+int DynamicScheduler::schedule(ScheduleType type) {
+	switch(type) {
+
+		case FIFO:
+			
+		break;
+		
+		case Operations:
+		
+		break;
+		
+		case CriticalPath:
+		
+		break;
+		
+		case ResourcesNeeded:
+		
+		break;
+		
+		default:
+			// FIFO
+		break;
+	}
+	return 0;
 }
